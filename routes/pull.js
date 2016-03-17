e27b243a76b818ac69433739d82db77a743d1a2c
Vitalii Petrenko <zenway.js@gmail.com>
2016-03-17 18:59:47 +0200

    Add express

--- routes/pull.js
+++ routes/pull.js
@@ -3,17 +3,17 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.pull = function(req, res){
   const childProcess = require('child_process');
   const git = childProcess.exec('cd ../myocard.io-react/ && git pull origin test');
   // const git = childProcess.exec('cd ../myocard.io-react/ && git status');
 
   git.stdout.on('data', (data) => {
-    res.render('index', { title: 'Obsidian Test Server', data: data });
+    res.render('pull', { title: 'Obsidian Test Server', data: data });
   });
 
   git.stderr.on('data', (data) => {
-    res.render('index', { title: 'Obsidian Test Server', data: data });
+    res.render('pull', { title: 'Obsidian Test Server', data: data });
   });
 
   git.on('close', (code) => {
